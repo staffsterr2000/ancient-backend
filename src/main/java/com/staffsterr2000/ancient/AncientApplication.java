2a1faed891536d3267ff61bd9970f4d31dd1cdfb
staffsterr2000 <rosenkostas@gmail.com>
2022-03-26 22:48:04 +0200

    init commit

---
+++ src/main/java/com/staffsterr2000/ancient/AncientApplication.java
@@ -0,0 +1,13 @@
+package com.staffsterr2000.ancient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AncientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staffsterr2000/ancient/AncientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffsterr2000.ancient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
